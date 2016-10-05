0cf3b1bd006f638ec3d693d4245e66580fb194b1
Pavel Studenik <pstudeni@redhat.com>
2016-10-05 14:37:18 +0200

    create loca repo with rpm tests

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,6 @@
 
 from fabric.api import env, get, run, cd
 
-env.hosts = [""]
 env.user = "root"
 env.password = "spacewalk"
 
@@ -12,6 +11,15 @@ env.hosts = open("config/hosts.ini").read().split()
 # Run test on remote guest. For example:
 # fab run_test:test_name=/CoreOS/Spacewalk/Others/Example
 def run_test(test_name):
-	with cd("/mnt/tests/"):
-		run("make build -C ./%s" % test_name)
-		run("make -C ./%s" % test_name)
+    with cd("/mnt/tests/"):
+            run("make build -C ./%s" % test_name)
+            run("make -C ./%s" % test_name)
+
+
+def build_all():
+    with cd("/mnt/tests/"):
+        repo_path = "/var/repos/beaker-test"
+        run("for it in $( find ./ -name Makefile ); do make rpm -C $( dirname ${it}); done")
+        run("mkdir -p %s; for it in $( find ./ -name 'spacewalk-test*.rpm'); do mv $it %s; done" % (repo_path, repo_path))
+        run("createrepo %s" % repo_path)
+        run("dnf config-manager --add %s && dnf repolist" % repo_path)
