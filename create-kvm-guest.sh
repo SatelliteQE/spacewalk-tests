be6463a78d00b9db5923e0831941322c20a664c6
Jan Hutar <jhutar@redhat.com>
2016-10-10 14:20:56 +0200

    Fix install helper

--- create-kvm-guest.sh
+++ create-kvm-guest.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 
 release=${release:="24"}
+guest="fedora-${release}-spacewalk"
 echo "==========================="
 echo "    Install Fedora $release"
 dnf install -y virt-install virt-viewer
 virt-install \
-   --name="fedora-${release}-spacewalk" \
-   --disk path="/var/lib/libvirt/images/fedora-${release}-guest-1.dsk",size=8,sparse=false,cache=none \
+   --name="${guest}" \
+   --disk path="/var/lib/libvirt/images/${guest}-1.dsk",size=8,sparse=false,cache=none \
    --graphics spice \
    --vcpus=2 --ram=2048 \
-   --location="http://download.eng.brq.redhat.com/pub/fedora/linux/releases/${release}/Server/x86_64/os" \
+   --location="https://download.fedoraproject.org/pub/fedora/linux/releases/${release}/Server/x86_64/os/" \
    --network network:default \
     -x "ks=https://raw.githubusercontent.com/SatelliteQE/spacewalk-tests/master/config/fedora.ks" \
    --os-type=linux \
    --os-variant=fedora23 \
    --noautoconsole
+if [ $? -ne 0 ]; then
+  echo "ERROR: Failed to install the virtual machine" >&2
+  exit 1
+fi
 
 # Now wait for installation to finish and start the host
 while true; do
-  virsh list | grep 'fedora-spacewalk.*running' && break
+  virsh list | grep "${guest}.*running" && break
   sleep 5
 done
 while true; do
-  virsh list --all | grep 'fedora-spacewalk.*shut off' && break
+  virsh list --all | grep "$guest.*shut off" && break
   sleep 15
 done
 virsh start fedora-spacewalk
 
 # Populate Ansible inventory
-mac=$( virsh dumpxml fedora-spacewalk | grep 'mac\s\+address=' | cut -d "'" -f 2 )
+mac=$( virsh dumpxml $guest | grep 'mac\s\+address=' | cut -d "'" -f 2 )
 ip=$( arp -n | grep -i "$mac" | cut -d " " -f 1 )
 echo "$ip" >config/hosts.ini
